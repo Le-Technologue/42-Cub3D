7c12c6f5fd72c0cccb90657041dcd85e56cf776b
William Etievent Cazorla <wetieven@student.42lyon.fr>
2021-11-26 16:10:44 +0100

    Slightly more drafting ... slow code day today.

--- srcs/data/cub_data.c
+++ srcs/data/cub_data.c
@@ -6,7 +6,7 @@
 /*   By: wetieven <wetieven@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 09:34:22 by wetieven          #+#    #+#             */
-/*   Updated: 2021/11/26 12:04:03 by wetieven         ###   ########lyon.fr   */
+/*   Updated: 2021/11/26 16:10:02 by wetieven         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,18 @@ t_error	cub_parsing(t_game *game, const char *cub_line)
 		[CEI] = {.flag = "C", .fct = &color, .ctnt = NULL},
 		[MAP] = {NULL, .fct = &cub_parse_map, .ctnt = NULL},
 	};
+	int				elem;
+
+	while (cub[elem].flag)
+	{
+		if (!ft_strncmp(cub[elem].flag, cub_line, 2))
+			if (!cub[elem].ctnt)
+				(cub[elem].fct())
+			else
+				return (ft_err_msg("Duplicate cub element", PARSE));
+			
+
+
+		elem++;
+	}
 }
