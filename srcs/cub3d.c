f0ec2a3bcc49dad107404ae3a7a12bc8a24341b3
William Cazorla <wetieven@student.42lyon.fr>
2021-12-02 21:48:27 +0100

    Getting an idea for that parsing loop... ...slow days these were, but maybe it is time to use a functor to recycle the read_cub function to finish that parsing logic.

--- srcs/cub3d.c
+++ srcs/cub3d.c
@@ -6,8 +6,32 @@
 /*   By: wetieven <wetieven@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 11:43:29 by wetieven          #+#    #+#             */
-/*   Updated: 2021/11/10 11:43:31 by wetieven         ###   ########lyon.fr   */
+/*   Updated: 2021/12/02 17:43:22 by wetieven         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+static t_error	cub_read_conf(t_game *game, const char *cub_path, )
+{
+	int		fd;
+	char	*cub_line;
+	t_error	error;
+	int		gnl_status;
 
+	if (fd_opener(cub_path, &fd) != CLEAR)
+	{
+		ft_printf("Error\nThe map file path \"%s\" leads nowhere\n", map_path);
+		return (FD_OPENING);
+	}
+	error = CLEAR;
+	while (!error)
+	{
+		gnl_status = get_next_line(fd, &cub_line);
+		if (gnl_status >= 0)
+			error = cub_parsing(game, cub_line, gnl_status);
+		free(map_line);
+		if (gnl_status <= 0)
+			break ;
+	}
+	fd_killer(fd);
+	return (error);
+}
--- srcs/data/cub_data.c
+++ srcs/data/cub_data.c
@@ -6,15 +6,42 @@
 /*   By: wetieven <wetieven@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 09:34:22 by wetieven          #+#    #+#             */
-/*   Updated: 2021/11/30 11:03:37 by wetieven         ###   ########lyon.fr   */
+/*   Updated: 2021/12/02 17:42:25 by wetieven         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub_data.h"
 
-t_error	cub_parsing(t_game *game, const char *cub_line)
+bool	is_cub_map_elem(char c)
 {
-	static int		to_parse = CUB_DATA_RANGE;
+	if (c == ' ' || c == '1' || c == '0'
+		|| c == 'N' || c == 'W' || c == 'S' || c == 'E')
+		return (true);
+	else
+		return (false);
+}
+
+t_error	cub_chk(t_cub *cub)
+{
+	int	elem;
+
+	elem = CUB_DATA_RANGE;
+	while (elem--)
+		if (cub[elem].ctnt == NULL)
+			return (PARSE);
+	return (CLEAR);
+}
+
+t_error	map_parsing(t_game *game, const char *cub_line, int gnl_status)
+{
+	if (!is_cub_map_elem(*cub_line));
+		return (ft_err_msg("Syntax error in cub file.", PARSE));
+	if (cub_chk(game->data) != CLEAR)
+		return (ft_err_chk("Incomplete cub file.", PARSE));
+}
+
+t_error	cub_parsing(t_game *game, const char *cub_line, int gnl_status)
+{
 	static t_cub	cub[] = {
 		[NOR] = {.flag = "NO", .fct = &textr, .ctnt = NULL},
 		[WES] = {.flag = "WE", .fct = &textr, .ctnt = NULL},
@@ -26,7 +53,7 @@ t_error	cub_parsing(t_game *game, const char *cub_line)
 	int				elem;
 
 	elem = CUB_DATA_RANGE;
-	while (to_parse && elem--)
+	while (*cub_line != '\0' && elem--)
 	{
 		if (ft_strncmp(cub[elem].flag, cub_line, 2) == MATCH)
 			if (!cub[elem].ctnt)
@@ -34,8 +61,10 @@ t_error	cub_parsing(t_game *game, const char *cub_line)
 			else
 				return (ft_err_msg("Extraneous cub element.", PARSE));
 	}
-	if (*cub_line != '\0')
-		return (ft_err_msg("Syntax error in cub file.", PARSE));
+	if (gnl_status == 0 && *cub_line == '\0')
+		return (PARSE); //If we haven't got ourselves a map at that stage, this is fucked.
+	else if (*cub_line == '\0')
+		return (CLEAR); //just an empty line
 	else
-		return (cub_parse_map()); //delegate missing cub elem mgmt to that func
+		return (cub_parse_map(game, gnl_status)); //delegate missing cub elem mgmt, and the bogus line check
 }
